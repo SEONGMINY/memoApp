fa400d34fe0c0ea4c55a4e5d907ead97ecd982ca
imzzuu <su_bin0619@naver.com>
2024-04-22 20:26:49 +0900

    chore: path alias 적용 및 typescript option 설정

--- pulip/babel.config.js
+++ pulip/babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'module-resolver',
+    {
+      root: ['./src'],
+      alias: {
+        '@apis': './src/apis',
+        '@assets': './src/assets',
+        '@components': './src/components',
+        '@constants': './src/constants',
+        '@hooks': './src/hooks',
+        '@modules': './src/modules',
+        '@queries': './src/queries',
+        '@screens': './src/screens',
+        '@stacks': './src/stacks',
+        '@states': './src/states',
+        '@styles': './src/styles',
+        '@models': './src/types',
+        '@utils': './src/utils',
+        '@': './src',
+      },
+    },
+  ],
 };
