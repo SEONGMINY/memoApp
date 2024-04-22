22c09a76e0d3a233cbd516e43c9eec308db8d5cc
imzzuu <su_bin0619@naver.com>
2024-04-22 20:14:15 +0900

    chore: eslint, prettier 적용

--- pulip/.eslintrc.js
+++ pulip/.eslintrc.js
@@ -1,4 +1,6 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['@typescript-eslint', 'prettier'],
+  parser: '@typescript-eslint/parser',
 };
--- pulip/.prettierrc.js
+++ pulip/.prettierrc.js
@@ -1,7 +1,20 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
   singleQuote: true,
   trailingComma: 'all',
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 100,
+  semi: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  overrides: [
+    {
+      files: '*.json',
+      options: {
+        printWidth: 200,
+      },
+    },
+  ],
 };
