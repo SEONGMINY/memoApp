49a12c105d81304a889642f047d22656184dd440
imzzuu <su_bin0619@naver.com>
2024-04-22 20:46:32 +0900

    chore: babel config, path alias 수정

--- pulip/babel.config.js
+++ pulip/babel.config.js
@@ -1,25 +1,27 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    'module-resolver',
-    {
-      root: ['./src'],
-      alias: {
-        '@apis': './src/apis',
-        '@assets': './src/assets',
-        '@components': './src/components',
-        '@constants': './src/constants',
-        '@hooks': './src/hooks',
-        '@modules': './src/modules',
-        '@queries': './src/queries',
-        '@screens': './src/screens',
-        '@stacks': './src/stacks',
-        '@states': './src/states',
-        '@styles': './src/styles',
-        '@models': './src/types',
-        '@utils': './src/utils',
-        '@': './src',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@apis': './src/apis',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@modules': './src/modules',
+          '@queries': './src/queries',
+          '@screens': './src/screens',
+          '@stacks': './src/stacks',
+          '@states': './src/states',
+          '@styles': './src/styles',
+          '@models': './src/types',
+          '@utils': './src/utils',
+          '@': './src',
+        },
       },
-    },
+    ],
   ],
 };
